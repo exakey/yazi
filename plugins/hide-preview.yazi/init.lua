38e9ba0b63c75df82f5cdf457a38fb1b390c9b3f
q <imran.mamedov.03@gmail.com>
2025-01-28 21:16:18 +0400

    --allow-empty-message

--- plugins/hide-preview.yazi/init.lua
+++ plugins/hide-preview.yazi/init.lua
@@ -1,24 +1,23 @@
 --- @sync entry
 
 local function entry(st)
-    if st.old then
-        Tab.layout, st.old = st.old, nil
-    else
-        st.old = Tab.layout
-        ---@diagnostic disable-next-line: duplicate-set-field
-        Tab.layout = function(jobs)
-            local all = MANAGER.ratio.parent + MANAGER.ratio.current
-            jobs._chunks = ui.Layout()
-                :direction(ui.Layout.HORIZONTAL)
-                :constraints({
-                    ui.Constraint.Ratio(MANAGER.ratio.parent, all),
-                    ui.Constraint.Ratio(MANAGER.ratio.current, all),
-                    ui.Constraint.Length(1),
-                })
-                :split(jobs._area)
-        end
-    end
-    ya.app_emit("resize", {})
+	if st.old then
+		Tab.layout, st.old = st.old, nil
+	else
+		st.old = Tab.layout
+		Tab.layout = function(self)
+			local all = MANAGER.ratio.parent + MANAGER.ratio.current
+			self._chunks = ui.Layout()
+				:direction(ui.Layout.HORIZONTAL)
+				:constraints({
+					ui.Constraint.Ratio(MANAGER.ratio.parent, all),
+					ui.Constraint.Ratio(MANAGER.ratio.current, all),
+					ui.Constraint.Length(1),
+				})
+				:split(self._area)
+		end
+	end
+	ya.app_emit("resize", {})
 end
 
 local function enabled(st) return st.old ~= nil end
